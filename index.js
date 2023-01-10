ff5769b2e01ec33ec583f8e3822c9a5a1d2adadd
SergeyChelombitko <113838183+SergeyChelombitko@users.noreply.github.com>
2023-01-10 15:38:20 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,100 @@
+let ceil=document.querySelectorAll(".game-item"),
+reset=document.querySelector("#reset-game"),
+message=document.querySelector("#message"),
+player="X",
+stepCount=0,
+
+
+winCombinations=[
+    [1,2,3],
+    [1,4,7],
+    [1,5,9],
+    [2,5,8],
+    [3,6,9],
+    [3,5,7],
+    [4,5,6],
+    [7,8,9]
+],
+
+dataX=[],
+data0=[];
+
+console.log(winCombinations.length)
+
+
+function currentStep() {
+    // console.log('data0: ' + data0.length)
+    // console.log('dataX: ' + dataX.length)
+    let num = +this.getAttribute("data-ceil");
+    if (!this.textContent) {
+        this.innerText = player;
+        player === "X"
+        // console.log(player)
+        ? dataX.push(num) && this.classList.add("x")
+        : data0.push(num) && this.classList.add("o");
+        if (
+            (data0.length == 2) &
+            (checkWin(data0, num) || checkWin(dataX, num))
+        )
+        {
+            console.log('test')
+            console.log(num)
+            console.log(data0)
+            console.log(dataX)
+
+            for (let i = 0; i < ceil.length; i++){
+                ceil[i].removeEventListener("click", currentStep);
+
+            }
+            return (message.innerText = "Победил игрок" + player);
+        }
+        changePlayer();
+        stepCount++;
+        stepCount === 9
+            ? (message.innerText = "Ничья")
+            : (message.innerText = "Ходит игрок" + player);
+    }
+}
+for (let i = 0; i < ceil.length; i++){
+    ceil[i].addEventListener("click", currentStep);
+}
+function changePlayer(){
+    player === "X" ? (player = "O") : (player = "X");
+}
+reset.addEventListener("click", function() {
+    for (let i = 0; i < ceil.length; i++){
+        ceil[i].innerText = "";
+    }
+    data0 = [];
+    dataX = [];
+    player = "O"
+    stepCount = 0;
+    message.innerText = "Ходит игрок " + player;
+    for (let i =0; i < ceil.length; i++){
+        ceil[i].addEventListener("click", currentStep);
+        ceil[i].classList.remove("x", "o");
+    }   
+});
+function checkWin(arr, number){
+    for (let w = 0, wLen = winCombinations.length; w < wLen; w++){
+        let someWinArr = winCombinations[w],
+        count = 0;
+
+        // console.log(someWinArr)
+        // console.log(count)
+        // console.log(someWinArr.indexOf(number))
+
+    if (someWinArr.indexOf(number) !== -1){
+        for (let k = 0, kLen = someWinArr, length; k < kLen; k++){
+            if (arr.indexOf(someWinArr[k]) !== -1){
+                count++;
+                console.log(count)
+                if (count === 3){
+                    return true;
+                }
+            }
+        }
+        count = 0;    
+    }    
+  }
+}
